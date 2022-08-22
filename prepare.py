c0bd5ff18da58bc0cffd4283e6d7ae12f90c9e8a
David Howell <davidallenhowell@gmail.com>
2022-08-22 17:02:22 -0500

    update prepare

--- prepare.py
+++ prepare.py
@@ -85,6 +85,15 @@ def split_data(df, strat_by, rand_st=123):
 #     train test split to break out a validation set the same size as the test set
     train, validate = train_test_split(train, test_size=.25, 
                  random_state=rand_st, stratify=train[strat_by])
+#     Remove extraneous training data:
+    # get the index of the one year contract drops
+    one_year_drops = train[(train.one_year == 1)&(train.tenure < 13)].index
+    # get the index of the two year contract drops
+    two_year_drops = train[(train.two_year == 1)&(train.tenure < 25)].index
+    # cast both.tolist() and addd them together to have an index to drop from train
+    contract_drops = one_year_drops.tolist()+two_year_drops.tolist()
+    train.drop(index=contract_drops, inplace=True)
+
 #     print the name and shape of the resultant DataFrames
     print(f'Prepared df: {df.shape}')
     print()
@@ -94,3 +103,60 @@ def split_data(df, strat_by, rand_st=123):
 
 #     returns the resulting DataFrames in order of train, validate, test 
     return train, validate, test
+
+def fix_nulls():
+    df = acquire.get_telco_data()
+#     drop_columns = ['gender', 'phone_service', 'multiple_lines', 'streaming_tv', 'streaming_movies']
+    df.total_charges = pd.to_numeric(df.total_charges, errors='coerce')
+    df['null_charges'] = pd.to_numeric(df['total_charges'], errors='coerce').isnull()
+
+    df['total_charges'][df['null_charges'] == True] = df['monthly_charges'][df['null_charges'] == True]
+    return df
+
+
+def prep_telco_1(train):
+    train['churn1'] = train.churn == "Yes"
+    train['churn1'] = train['churn1'].map({False: 0, True:1})
+    
+    # copy paste and fix the typo
+    drop_columns = ['gender',
+     'phone_service',
+     'multiple_lines',
+     'streaming_tv',
+     'streaming_movies',
+     'churn',
+     'null_charges',
+     'customer_id']
+    train.drop(columns=drop_columns, inplace=True)
+    for i in train[['partner', 'dependents', 
+       'online_security', 
+       'online_backup', 
+       'device_protection', 
+       'tech_support', 
+       'paperless_billing']]:
+        train[i] = train[i].map({'Yes': 1, 'No': 0, 'No internet service': 0})
+    dummy_list = ['internet_service_type', 'contract_type', 'payment_type']
+    dummies = pd.get_dummies(train[dummy_list])
+    train = pd.concat([train, dummies], axis=1)
+    train['add_ons'] = train.online_backup + train.online_security + train.device_protection + train.tech_support
+    drop_dummies = ['internet_service_type', 
+               'online_security', 
+               'online_backup', 
+               'device_protection', 
+               'tech_support',
+               'contract_type',
+               'payment_type',
+               'internet_service_type_None',
+               'contract_type_Month-to-month',
+               'payment_type_Mailed check'
+              ]
+    train.drop(columns=drop_dummies, inplace=True)
+    train.columns = ['senior', 'partner', 'dependents', 'tenure',
+       'e_bill', 'monthly_charges', 'total_charges', 'churn1',
+       'DSL', 'Fiber',
+       'one_year', 'two_year',
+       'bank_transfer',
+       'cc', 'e_check',
+       'add_ons']
+    return train
+    
